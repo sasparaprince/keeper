7cd9a1b585925efe1deed95b0003581e79546922
prince saspara <princesaspara46@gmail.com>
2023-08-17 20:05:12 +0530

    add notes array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+
+
+      {notes.map((noteItems) => {
+    return(
+      <Note key={noteItems.key} title={noteItems.title} content={noteItems.content}  />
+
+    )
+      })}
+
       <Footer />
     </div>
   );
--- src/notes.js
+++ src/notes.js
@@ -3,5 +3,22 @@ const notes = [
         key: 1 , 
         title: 'first',
         content: 'this is first note '
-    }
+    },
+    {
+        key: 2 , 
+        title: 'second',
+        content: 'this is second note '
+    },
+    {
+        key: 3 , 
+        title: 'third',
+        content: 'this is third note '
+    },
+    {
+        key: 4 , 
+        title: 'fourth',
+        content: 'this is fourth note '
+    },
 ]
+
+export default notes
